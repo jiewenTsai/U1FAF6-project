4d357edf4d5c7f39a8f1d34523c5125a12f73bfc
Jiw-Wen Tsai <44721885+jiewenTsai@users.noreply.github.com>
2025-02-16 18:41:34 +0800

    0216

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -1,14 +1,13 @@
-if (!window.location.pathname.includes('.ipynb')) {
-  window.MathJax = {
-      tex: {
-          inlineMath: [['$', '$'], ['\\(', '\\)']],
-          displayMath: [['$$', '$$'], ['\\[', '\\]']],
-          processEscapes: true,
-          processEnvironments: true
-      },
-      options: {
-          ignoreHtmlClass: ".*|",
-          processHtmlClass: "arithmatex"
-      }
-  };
-}
+
+window.MathJax = {
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
